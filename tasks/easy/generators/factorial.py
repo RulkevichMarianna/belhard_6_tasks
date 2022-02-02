5d4787478614a7ed9b3903271c08abafaba5743c
Maryana Rulkevich <obrazcova.m@gmail.com>
2022-02-03 01:04:54 +0300

    done factorial

--- tasks/easy/generators/factorial.py
+++ tasks/easy/generators/factorial.py
@@ -10,3 +10,12 @@ next(factorial_gen) -> 2
 next(factorial_gen) -> 6
 next(factorial_gen) -> 24
 """
+
+
+def factorial():
+    current = 1
+    result = 1
+    while True:
+        result *= current
+        yield result
+        current += 1
